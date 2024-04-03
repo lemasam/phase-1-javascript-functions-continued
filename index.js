88be409d297964903a743b0d482b094d0f70e01c
lema samuel <lemasam10@gmail.com>
2024-04-03 10:47:20 +0300

    function Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(action = "go to the office") {
+  return `This Monday, I will ${action}.`;
+}
+function wrapAdjective(wrapper) {
+  return function (adjective = "a dedicated programmer") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
